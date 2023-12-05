aa1988cf9c1179d6e5c4f1f7648ed75bf43a2a3b
naoya0117 <81454816+naoya0117@users.noreply.github.com>
2023-12-05 09:02:51 +0900

    検索機能を追加

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -1,8 +1,8 @@
 <form method="get">
     {{ csrf_field() }}
     <div class='form-group'>
-        <label id='keyword'>キーワード:</label>
-        <input type="text" name="keyword" class="form-control">
+        <label for="keyword">キーワード:</label>
+        <input type="text" id="keyword" name="keyword" class="form-control">
     </div>
     <div class='form-group'>
         <input type="submit" value="検索" class="btn btn-outline-primary">
