28ebdfa61667ef72c590cf04c0e35dcf408b1231
naoya0117 <81454816+naoya0117@users.noreply.github.com>
2023-12-05 09:32:09 +0900

    ルーティングを追加

--- routes/web.php
+++ routes/web.php
@@ -20,5 +20,10 @@ Route::get('/', function () {
 
 Route::get('/articles', [ArticleController::class, 'index'])->name('article.list');
 Route::get('/article/new', [ArticleController::class, 'create'])->name('article.new');
+Route::post('/article', [ArticleController::class, 'store'])->name('article.store');
+
+Route::get('/article/edit/{id}', [ArticleController::class, 'edit'])->name('article.edit');
 Route::get('/article/{id}', [ArticleController::class, 'show'])->name('article.show');
+Route::post('/article/update/{id}', [ArticleController::class, 'update'])->name('article.update');
+
 Route::delete('/article/{id}', [ArticleController::class, 'destroy'])->name('article.delete');
